5e56269b1789f093da2d234ec9fef23bc4e8090b
Charlie9009 <charlie.gustafsson90@gmail.com>
2024-07-30 10:42:18 +0000

    start fixing the top container, styling

--- assets/todo.js
+++ assets/todo.js
@@ -1,2 +1,10 @@
-let selector = document.querySelector('h1')
-selector.style.color = 'blue'
+let upperContainer = document.querySelector('.upper-container')
+upperContainer.style.backgroundColor = 'red'
+upperContainer.style.display = 'flex';
+upperContainer.style.justifyContent = 'center';
+upperContainer.style.alignItems = 'center';
+upperContainer.style.margin = '20px'
+upperContainer.style.borderRadius = '20px'
+
+
+// upperContainer.style.color = 'white'
