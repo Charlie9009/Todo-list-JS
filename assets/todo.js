bce738fd7d90e17d6d9a2cb63eb375291875b260
Charlie9009 <charlie.gustafsson90@gmail.com>
2024-07-30 11:43:41 +0000

    Add another container for the h1 for styling purposes, add logic in js

--- assets/todo.js
+++ assets/todo.js
@@ -13,10 +13,30 @@ const styleUpperContainer = () => {
 
 
 const styleInputContainer = () => {
-    
-let inputContainer = document.querySelector('.input-container');
-inputContainer.style.margin = '20px 0px';
+
+    let inputContainer = document.querySelector('.input-container');
+    inputContainer.style.display = 'flex';
+    inputContainer.style.justifyContent = 'center';
+    inputContainer.style.alignItems = 'center';
+    inputContainer.style.margin = '20px 0px';
+    inputContainer.style.width = '100%';
+    inputContainer.style.textAlign = 'center';
 }
+
+let h1Container = document.querySelector('.h1-container')
+h1Container.style.width = '100%';
+h1Container.style.textAlign = 'center';
+
+let input = document.querySelector('input');
+input.style.padding = '10px';
+input.style.width = '60%';
+
+
+let button = document.querySelector('button');
+button.style.padding = '10px';
+button.style.width = '20%';
+
+
 
 
 styleUpperContainer();
