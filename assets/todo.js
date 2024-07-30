0c195136ef5b2f4072dcb590c0f4c17c7515d4c4
Charlie9009 <charlie.gustafsson90@gmail.com>
2024-07-30 10:44:37 +0000

    Put logic in a function

--- assets/todo.js
+++ assets/todo.js
@@ -1,10 +1,19 @@
-let upperContainer = document.querySelector('.upper-container')
-upperContainer.style.backgroundColor = 'red'
-upperContainer.style.display = 'flex';
-upperContainer.style.justifyContent = 'center';
-upperContainer.style.alignItems = 'center';
-upperContainer.style.margin = '20px'
-upperContainer.style.borderRadius = '20px'
+
+const styleUpperContainer = () => {
+
+    let upperContainer = document.querySelector('.upper-container')
+    
+    upperContainer.style.backgroundColor = 'red'
+    upperContainer.style.display = 'flex';
+    upperContainer.style.justifyContent = 'center';
+    upperContainer.style.alignItems = 'center';
+    upperContainer.style.margin = '20px'
+    upperContainer.style.borderRadius = '20px'
+}
+
+
+
+styleUpperContainer()
 
 
 // upperContainer.style.color = 'white'
