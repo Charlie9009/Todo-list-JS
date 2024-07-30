916084831ffd570a9b58ee9c77381983d90c1875
Charlie9009 <charlie.gustafsson90@gmail.com>
2024-07-30 11:01:45 +0000

    Add styling for inputcontainer and put it in a function

--- assets/todo.js
+++ assets/todo.js
@@ -1,19 +1,25 @@
 
 const styleUpperContainer = () => {
 
-    let upperContainer = document.querySelector('.upper-container')
-    
-    upperContainer.style.backgroundColor = 'red'
+    let upperContainer = document.querySelector('.upper-container');
+
+    upperContainer.style.backgroundColor = 'red';
     upperContainer.style.display = 'flex';
     upperContainer.style.justifyContent = 'center';
     upperContainer.style.alignItems = 'center';
-    upperContainer.style.margin = '20px'
-    upperContainer.style.borderRadius = '20px'
+    upperContainer.style.margin = '20px';
+    upperContainer.style.borderRadius = '20px';
 }
 
 
+const styleInputContainer = () => {
+    
+let inputContainer = document.querySelector('.input-container');
+inputContainer.style.margin = '20px 0px';
+}
 
-styleUpperContainer()
 
+styleUpperContainer();
+styleInputContainer();
 
 // upperContainer.style.color = 'white'
