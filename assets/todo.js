2df218ada2b3babff7a922eab078d7cb8275880e
Charlie9009 <charlie.gustafsson90@gmail.com>
2024-07-31 10:52:58 +0000

    Add close button and functionality for close button

--- assets/todo.js
+++ assets/todo.js
@@ -1,8 +1,10 @@
 
 const newListItem = () => {
+    // Add todo item
     let li = document.createElement('li');
     let inputValue = document.querySelector('#myInput').value
     let text = document.createTextNode(inputValue);
+
     li.appendChild(text);
     if (inputValue === '') {
         alert('You did not write anything');
@@ -12,9 +14,22 @@ const newListItem = () => {
 
     }
     document.querySelector('#myInput').value = '';
-    
 
-    
+    // close icon for todos
+    let span = document.createElement('span');
+    let closeIcon = document.createTextNode('X');
+    let closeClassName = document.querySelectorAll('.close')
+
+    span.className = 'close';
+    span.appendChild(closeIcon);
+    li.appendChild(span);
+
+    for (let i = 0; i < closeClassName.length; i++) {
+        closeClassName[i].onclick = function () {
+            let div = this.parentElement;
+            div.style.display = "none";
+        }
+    }
 }
 
 newListItem()
